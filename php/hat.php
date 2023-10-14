5eba8b80068c39aeb324e675cfa86b3e870325f0
AlKharkov <79756667+AlKharkov@users.noreply.github.com>
2023-10-14 22:11:57 +0700

    Add files via upload

---
+++ php/hat.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Форум проектов</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="../js/bootstrap.js"></script>
+</head>
+<body>
+<section class="hat">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2">
+                <img class="hat-logo" src="../img/fp-logo.png" alt="Форум проектов">
+            </div>
+            <div class="hat-link-block offset-md-1 col-md-4">
+                <a class="hat-link" href="#">О нас</a>
+                <a class="hat-link" href="#">Проекты</a>
+                <a class="hat-link" href="#">Форум</a>
+                <a class="hat-link" href="#">Участники</a>
+            </div>
+            <div class="offset-md-1 col-md-4">
+                <button class="btn button-1 hat-btn">ВОЙТИ</button>
+                <button class="btn button-1 hat-btn">РЕГИСТРАЦИЯ</button>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
